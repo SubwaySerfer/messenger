bae14e3408b7e711705234946dcb6fa6d52a6850
Aleksander Afanasyev <afonia.marcii@gmail.com>
2023-10-15 22:40:09 +0300

    fix: add empty last line

--- src/modules/ErrorBlock/errorBlock.tmpl.js
+++ src/modules/ErrorBlock/errorBlock.tmpl.js
@@ -1,7 +1,11 @@
-export const tmpl = `
+export const tmpl = 
+`
  <div class="error-block">
  <h1 class="error-block__title">{{ errorTitle }}</h1>
  <h2 class="error-block__description">{{ errorDescription }}</h2>
  {{{ ButtonLink }}}
- </div> `
+
+ </div>
+ `
+
  
