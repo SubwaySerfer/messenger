0e1a6f3db71868ddf2ce88da7987f1835b36b51e
Aleksander Afanasyev <afonia.marcii@gmail.com>
2023-11-06 20:19:19 +0300

    init: start second sprint

--- src/modules/ErrorBlock/index.ts
+++ src/modules/ErrorBlock/index.ts
@@ -1,8 +1,8 @@
 import Handlebars from "handlebars";
 
 import { tmpl } from "./errorBlock.tmpl";
-
 import { ButtonLink } from "../../components/ButtonLink";
+
 
 export const ErrorBlock = ({ errorTitle, errorDescription }) => {
   return Handlebars.compile(tmpl)({errorTitle, errorDescription, 
