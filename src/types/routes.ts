b166c3c9b8b4aa15a1539c39e49fe367edadd2b3
Aleksander Afanasyev <afonia.marcii@gmail.com>
2023-12-27 16:33:37 +0300

    feat: add Router

---
+++ src/types/routes.ts
@@ -0,0 +1,10 @@
+export const enum ROUTES {
+  login = '/',
+  signup = '/sign-up',
+  chat = '/messenger',
+  profile = '/settings',
+  profileChangeData = '/settings-change-data',
+  profileChangePassword = '/settings-change-password',
+  page400 = '/page400',
+  page500 = '/page500',
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite';
 import handlebars from './vite-plugin-handlebars-precompile';
-
-console.log(handlebars);
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   plugins: [handlebars()],
 });
